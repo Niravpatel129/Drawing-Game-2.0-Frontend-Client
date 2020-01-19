0e3f23cbd4be684f12825954920a531250bfdb33
Niravpatel129 <niravpatelp129@gmail.com>
2020-01-18 21:26:54 -0500

    init

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -1,11 +1,41 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import "./Chat.scss";
+import Message from "../Message/Message";
 
 function Chat() {
+  const [messages, addMessage] = useState([]);
+  const [input, changeInput] = useState("");
+
+  const messagesRef = useRef();
+
+  useEffect(() => {
+    messagesRef.current.scrollTop = messagesRef.current.scrollHeight;
+  });
+
+  const submitMessage = e => {
+    if (e.charCode === 13) {
+      if (input) {
+        addMessage([
+          ...messages,
+          <Message message={input} key={messages.length + 1} />
+        ]);
+      }
+      changeInput("");
+    }
+  };
+
   return (
     <div className="Chat">
-      <div className="message"></div>
-      <input></input>
+      <div className="messages" ref={messagesRef}>
+        {messages.map((e, index) => {
+          return e;
+        })}
+      </div>
+      <input
+        value={input}
+        onChange={e => changeInput(e.target.value)}
+        onKeyPress={submitMessage}
+      ></input>
     </div>
   );
 }
--- src/components/Message/Message.js
+++ src/components/Message/Message.js
@@ -1,22 +1,29 @@
 import React from "react";
 import "./Message.scss";
 
-function Message() {
+function Message({ message }) {
+  const generateRandomNumber = () => {
+    return Math.random() * (255 - 0) + 0;
+  };
   return (
-    <div className="Message">
-      <div className="chatContainer">
-        <div className="container">
-          <h1>Hello</h1>
-          <img
-            src="https://www.w3schools.com/w3images/bandmember.jpg"
-            alt="Avatar"
-          />
-          <p>Sweet! So, what do you wanna do today?????????</p>
+    <>
+      <div className="MessageContainer">
+        <img
+          src="https://www.w3schools.com/w3images/bandmember.jpg"
+          alt="https://www.w3schools.com/w3images/bandmember.jpg"
+        ></img>
+        <div className="Message">
+          <h4
+            style={{
+              color: `rgba(${generateRandomNumber()}, ${generateRandomNumber()}, ${generateRandomNumber()})`
+            }}
+          >
+            RedSkyez
+          </h4>
+          <p>{message || "You need to enable JavaScript to run this app."}</p>
         </div>
       </div>
-
-      <input placeholder="Send"></input>
-    </div>
+    </>
   );
 }
 
